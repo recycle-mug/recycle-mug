551904a846457014cd01d5bf5a3c6eebbf6e3c66
wotj7687 <wotj7687@icloud.com>
2021-05-23 21:13:19 +0900

    add MemberRepositoryTest.java

---
+++ Project-Mug/src/test/java/recyclemug/ProjectMug/domain/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package recyclemug.ProjectMug.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember(){
+        Member member = new Member();
+        member.setName("memberA");
+
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+    }
+
+}
