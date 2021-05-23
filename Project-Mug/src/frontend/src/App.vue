c346318d4c6d38d6d90e1ae857709a9c67ea5024
TorryTheMaltese <torryiscute@gmail.com>
2021-05-23 16:17:00 +0900

    build project structure

---
+++ Project-Mug/src/frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view :key="this.$route.fullPath"></router-view>
+</template>
+
+<script>
+import router from "./routes/routes.js";
+import store from "./stores/store";
+
+export default {
+  router,
+  store,
+};
+</script>
+
+<style lang="scss" scoped></style>
