5a52458189dfab6b0e22b2e6acd014a19923889b
wotj7687 <wotj7687@icloud.com>
2021-05-23 16:00:57 +0900

    initialize commit

---
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/ProjectMugApplication.java
@@ -0,0 +1,13 @@
+package recyclemug.ProjectMug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMugApplication.class, args);
+	}
+
+}
---
+++ Project-Mug/src/test/java/recyclemug/ProjectMug/ProjectMugApplicationTests.java
@@ -0,0 +1,13 @@
+package recyclemug.ProjectMug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
