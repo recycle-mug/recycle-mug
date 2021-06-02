f5eaea0b32c58fa77e05e2c3f0bd7b2655d39ee8
TorryTheMaltese <torryiscute@gmail.com>
2021-06-03 06:01:15 +0900

    load map with current location

--- Project-Mug/src/frontend/src/components/GetKakaoMap.vue
+++ Project-Mug/src/frontend/src/components/GetKakaoMap.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="container">
+    <div v-if="locationErr">위치 정보를 찾을 수 없습니다.</div>
+    <div v-if="gettingLocation">위치 정보 조회 중...</div>
+
     <div id="map" style="width:500px;height:500px;" class="map-container"></div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      location: null,
+      gettingLocation: false,
+      locationErr: null,
+    };
+  },
   methods: {
     onLoadMap() {
       const container = document.getElementById("map");
-      const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
-      };
+      let options;
+      if (this.location) {
+        options = {
+          center: new kakao.maps.LatLng(
+            this.location.coords.latitude,
+            this.location.coords.longitude,
+          ),
+          level: 3,
+        };
+      } else {
+        options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+      }
 
       const map = new kakao.maps.Map(container, options);
-
-      console.log("map :>> ", map);
     },
     onAddScript() {
       const script = document.createElement("script");
@@ -24,12 +43,29 @@ export default {
       script.src =
         "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=6e95b4c59c1a9d01927e2946ff3a4c88";
       document.head.appendChild(script);
-
-      console.log("script :>> ", script);
     },
   },
   mounted() {
     window.kakao && window.kakao.maps ? this.onLoadMap() : this.onAddScript();
+  },
+  created() {
+    if (!("geolocation" in navigator)) {
+      this.locationErr = "위치 정보 권한이 없습니다";
+      return;
+    }
+
+    this.gettingLocation = true;
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.gettingLocation = false;
+        this.location = pos;
+        this.onLoadMap();
+      },
+      (err) => {
+        this.gettingLocation = false;
+        this.locationErr = err.message;
+      },
+    );
   },
 };
 </script>
