4a8f6d926c09a0dc8494cc645b705cc96a0f16b8
onAuspicious <wotj7687@icloud.com>
2021-06-30 00:53:59 +0900

    fix find, remove logic of cup (cup name -> cup id)

--- Project-Mug/src/main/java/recyclemug/ProjectMug/api/CustomerApiController.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/api/CustomerApiController.java
@@ -3,9 +3,7 @@ package recyclemug.ProjectMug.api;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import recyclemug.ProjectMug.domain.user.Customer;
 import recyclemug.ProjectMug.service.CustomerService;
 
