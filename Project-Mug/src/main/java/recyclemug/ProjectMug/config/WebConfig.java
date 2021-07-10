3c634bc798049dca9f8ceb5177d7db9f0249be9f
onAuspicious <wotj7687@icloud.com>
2021-07-11 02:20:59 +0900

    modify WebConfig

--- Project-Mug/src/main/java/recyclemug/ProjectMug/config/WebConfig.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods(
