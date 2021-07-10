ec72717e86fa519a495c1128c4c1c5c2900fc11a
onAuspicious <wotj7687@icloud.com>
2021-07-11 01:53:26 +0900

    modify data.sql

--- Project-Mug/src/main/resources/data.sql
+++ Project-Mug/src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO AUTHORITY values 'ROLE_CUSTOMER';
 INSERT INTO AUTHORITY values 'ROLE_PARTNER';
 INSERT INTO AUTHORITY values 'ROLE_ADMIN';
 
-insert into user (activated, email, nickname, password, phone_number, point, profile_picture, role, user_id) values (TRUE, 'admin', NULL, '{noop}admin', NULL, 1, NULL, 'A', 10000);
+insert into user (activated, email, nickname, password, phone_number, point, profile_picture_address, role, user_id) values (TRUE, 'admin', NULL, '{noop}admin', NULL, 1, NULL, 'A', 10000);
 insert into admin (user_id, admin_name) values (10000, 'WE-CUP');
 insert into user_authority (user_id, authority_name) values (10000, 'ROLE_ADMIN');
 
