b30a366dc4d833e95bc7f429e8bb75fadfd6b504
onAuspicious <wotj7687@icloud.com>
2021-07-15 17:59:37 +0900

    add CreatePartnerResponse.java

---
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/data/CreatePartnerResponse.java
@@ -0,0 +1,26 @@
+package recyclemug.ProjectMug.data;
+
+import lombok.Builder;
+import lombok.Data;
+import recyclemug.ProjectMug.domain.cup.PartnerCup;
+
+import java.util.List;
+
+@Data
+@Builder
+public class CreatePartnerResponse {
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private byte[] profilePicture;
+    private int point;
+    private String nickname;
+
+    // Partner only val
+    private String businessName;
+    private String zipcode;
+    private String detailAddress;
+    private String registrationNumber;
+
+    private List<PartnerCup> partnerCups;
+}
