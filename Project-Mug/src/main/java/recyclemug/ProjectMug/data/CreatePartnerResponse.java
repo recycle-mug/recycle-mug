a54a2f3f9cda0205003199b9777add8d6082c0bd
onAuspicious <wotj7687@icloud.com>
2021-07-15 23:23:23 +0900

    modify CreatePartnerResponse.java

--- Project-Mug/src/main/java/recyclemug/ProjectMug/data/CreatePartnerResponse.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/data/CreatePartnerResponse.java
@@ -1,7 +1,9 @@
 package recyclemug.ProjectMug.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import recyclemug.ProjectMug.domain.cup.PartnerCup;
 
 import java.util.List;
