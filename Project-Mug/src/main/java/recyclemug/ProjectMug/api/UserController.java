b256c673cab14f3da340bd07695e7c8accc84671
onAuspicious <wotj7687@icloud.com>
2021-07-15 17:48:26 +0900

    add methods in UserController.java (find All Users, All Customers, ALl Partners)

--- Project-Mug/src/main/java/recyclemug/ProjectMug/api/UserController.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/api/UserController.java
@@ -6,9 +6,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import recyclemug.ProjectMug.domain.user.Customer;
+import recyclemug.ProjectMug.domain.user.Partner;
 import recyclemug.ProjectMug.domain.user.User;
+import recyclemug.ProjectMug.repository.UserRepository;
 import recyclemug.ProjectMug.service.CustomerService;
 import recyclemug.ProjectMug.service.PartnerService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +21,29 @@ public class UserController {
 
     private final CustomerService customerService;
     private final PartnerService partnerService;
+    private final UserRepository userRepository;
 
     @GetMapping("/user/{email}")
     @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER')")
     public ResponseEntity<User> getUserInfo(@PathVariable String email) {
         return ResponseEntity.ok(customerService.getCustomerWithAuthorities(email).get());
+    }
+
+    @GetMapping("/user/findAllUsers")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public List<User> getAllUsers() {
+        return userRepository.findAllUsers();
+    }
+
+    @GetMapping("/user/findAllCustomers")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public List<Customer> getAllCustomers() {
+        return customerService.findCustomers();
+    }
+
+    @GetMapping("/user/findAllPartners")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public List<Partner> getAllPartners() {
+        return partnerService.findPartners();
     }
 }
