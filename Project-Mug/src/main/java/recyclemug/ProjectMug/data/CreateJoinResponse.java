58de731574cd4869b8a7cefc024eb5dca7d5cd9f
onAuspicious <wotj7687@icloud.com>
2021-07-16 19:16:21 +0900

    add CreateAllUserDataResponse.java, CreateJoinResponse.java

---
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/data/CreateJoinResponse.java
@@ -0,0 +1,11 @@
+package recyclemug.ProjectMug.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateJoinResponse {
+    private String result;
+    private String message;
+}
