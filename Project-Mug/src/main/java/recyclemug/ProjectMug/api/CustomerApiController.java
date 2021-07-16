b81fc911e370fd0585ebabe3f44d7394b1b2f002
onAuspicious <wotj7687@icloud.com>
2021-07-16 19:17:35 +0900

    modify annotation RestController to Controller

--- Project-Mug/src/main/java/recyclemug/ProjectMug/api/CustomerApiController.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/api/CustomerApiController.java
@@ -2,17 +2,17 @@ package recyclemug.ProjectMug.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import recyclemug.ProjectMug.data.CreateCustomerRequest;
-import recyclemug.ProjectMug.data.CreateCustomerResponse;
+import recyclemug.ProjectMug.data.CreateJoinResponse;
 import recyclemug.ProjectMug.domain.user.Customer;
 import recyclemug.ProjectMug.service.CustomerService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerApiController {
@@ -20,17 +20,18 @@ public class CustomerApiController {
     private final CustomerService customerService;
 
     @PostMapping("/join/customer")
-    public CreateCustomerResponse saveCustomer(@RequestBody @Valid CreateCustomerRequest request,
+    @ResponseBody
+    public CreateJoinResponse saveCustomer(@RequestBody @Valid CreateCustomerRequest request,
                                                HttpServletRequest httpServletRequest) {
         String picturePath = httpServletRequest.getServletContext().getRealPath("/images/users/default_user.jpg");
 
         try {
             Customer customer = Customer.createCustomer(request.getId(), request.getPw(), request.getTel(), picturePath);
             customerService.join(customer);
-            return new CreateCustomerResponse("success", "회원가입에 성공했습니다.");
+            return new CreateJoinResponse("success", "회원가입에 성공했습니다.");
         } catch (IllegalStateException e) {
             String message = e.toString();
-            log.error("FAIL TO JOIN PARTNER");
+            log.error("FAIL TO JOIN PARTNER: " + message);
         }
         return null;
     }
