0fc03ffa104e1eed8ca64c6fb96b1b6f0feb2d8b
onAuspicious <wotj7687@icloud.com>
2021-07-19 23:56:45 +0900

    add PartnerCupApiController.java and PartnerService.java

---
+++ Project-Mug/src/test/java/recyclemug/ProjectMug/service/PartnerServiceTest.java
@@ -0,0 +1,25 @@
+package recyclemug.ProjectMug.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import recyclemug.ProjectMug.ProjectMugApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ProjectMugApplication.class)
+@Transactional
+class PartnerServiceTest {
+
+    @Test
+    public void 파트너의컵추가() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+}
