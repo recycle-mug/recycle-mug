114eee058088f15ae693c6bdbac4fde495607b78
onAuspicious <wotj7687@icloud.com>
2021-10-11 00:12:49 +0900

    modify query : findCupOfPartner

--- Project-Mug/src/main/java/recyclemug/ProjectMug/repository/PartnerCupRepository.java
+++ Project-Mug/src/main/java/recyclemug/ProjectMug/repository/PartnerCupRepository.java
@@ -28,7 +28,10 @@ public class PartnerCupRepository {
      * @return
      */
     public List<PartnerCup> findCupOfPartner(Long partnerId) {
-        return em.createQuery("select pc from Partner p JOIN FETCH p.partnerCups pc WHERE p.id=:partnerId", PartnerCup.class)
+//        return em.createQuery("select pc from Partner p JOIN FETCH p.partnerCups pc WHERE p.id=:partnerId", PartnerCup.class)
+//                .setParameter("partnerId", partnerId)
+//                .getResultList();
+        return em.createQuery("select pc from PartnerCup pc join fetch p.partner p WHERE p.id=:partnerId", PartnerCup.class)
                 .setParameter("partnerId", partnerId)
                 .getResultList();
     }
